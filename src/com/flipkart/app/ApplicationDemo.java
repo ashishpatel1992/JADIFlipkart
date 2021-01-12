fa269b1d5047e322875a9ed216c6f823fbbd1ec2
Ashish Patel <ashishpatel.1992@gmail.com>
2021-01-12 15:01:32 +0530

    Java poject

---
+++ src/com/flipkart/app/ApplicationDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.app;
+
+/**
+ * @author Ashish
+ *
+ */
+public class ApplicationDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
